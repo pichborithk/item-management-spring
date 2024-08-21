864bf0a80cb0c1d03af3e4b32f7afbf2332dcee3
Pichborith Kong <pichborith@gmail.com>
2024-08-20 22:41:03 -0700

    remove unuse import

--- src/main/java/dev/pichborith/ItemManagement/controllers/AuthController.java
+++ src/main/java/dev/pichborith/ItemManagement/controllers/AuthController.java
@@ -8,7 +8,6 @@ import dev.pichborith.ItemManagement.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -23,9 +22,6 @@ public class AuthController {
 
     @Autowired
     private JwtService jwtService;
-
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(
