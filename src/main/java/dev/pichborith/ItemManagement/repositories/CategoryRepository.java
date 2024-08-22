bf5b5ceaae6d317415228837e7402b40fac90ae1
Pichborith Kong <pichborith@gmail.com>
2024-08-22 10:46:54 -0700

    change file/package path

--- src/main/java/dev/pichborith/ItemManagement/repositories/CategoryRepository.java
+++ src/main/java/dev/pichborith/ItemManagement/repositories/CategoryRepository.java
@@ -1,7 +1,8 @@
 package dev.pichborith.ItemManagement.repositories;
 
-import dev.pichborith.ItemManagement.models.Category;
+import dev.pichborith.ItemManagement.models.category.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
 }
