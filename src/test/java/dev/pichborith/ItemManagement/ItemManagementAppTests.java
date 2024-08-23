46f3ba8f13bb562f8c3edbe007e98cb22d1dd487
Pichborith Kong <pichborith@gmail.com>
2024-08-23 00:41:40 -0700

    remove unused test and update application.yml

--- src/test/java/dev/pichborith/ItemManagement/ItemManagementAppTests.java
+++ src/test/java/dev/pichborith/ItemManagement/ItemManagementAppTests.java
@@ -3,11 +3,11 @@ package dev.pichborith.ItemManagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ItemManagementAppTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class ItemManagementAppTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
